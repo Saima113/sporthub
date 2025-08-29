5d238977597f8e71adde051ca163912da0523fb8
saima <akramsaima459@gmail.com>
2025-08-29 23:31:17 +0530

    initial commit

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
-
-function connectDb(){
-  mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log('Successfully connected to db'));
-}
-
-module.exports = connectDb
